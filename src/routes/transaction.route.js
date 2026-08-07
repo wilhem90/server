924eb5e2a87cb47c4fb29ab4cbbee8485a0bc759
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2026-08-06 23:33:36 -0300

    UPDATE ROUTES

--- src/routes/transaction.route.js
+++ src/routes/transaction.route.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
   deposit,
   reviewTransaction,
-  transactionHistory,
   withdrawal,
 } from "../controllers/transaction.controller.js";
 import { critiqueRoute, privateRoute } from "../middlewares/auth.middleware.js";
@@ -12,6 +11,5 @@ const transactionRoutes = express.Router();
 transactionRoutes.post("/deposit", privateRoute, deposit);
 transactionRoutes.post("/withdrawal", privateRoute, withdrawal);
 transactionRoutes.post("/review", privateRoute, reviewTransaction);
-transactionRoutes.get("/history", privateRoute, transactionHistory);
 
 export default transactionRoutes;
--- src/routes/wallet.routes.js
+++ src/routes/wallet.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { showBalance } from "../controllers/wallet.controller.js";
 import { privateRoute } from "../middlewares/auth.middleware.js";
+import { transactionHistory } from "../controllers/transaction.controller.js";
 
 const walletRoutes = express.Router();
 walletRoutes.get("/balance", privateRoute, showBalance);
-walletRoutes.get("/extract-wallet", privateRoute, showBalance);
-
+walletRoutes.get("/history", privateRoute, transactionHistory);
 export default walletRoutes;
